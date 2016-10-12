259173d7f16cd3c6432d55e9db40987846c75225
Peterson <preston.peterson651@sie-1222-22.csuci.edu>
2016-10-12 14:34:03 -0700

    Added function to parse and resolve user requests

--- bot/BotClient.py
+++ bot/BotClient.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python
 
 import praw
-from prawoauth2 import PrawOAuth2Mini
+from bot.prawoauth2 import PrawOAuth2Mini
 import time
-
-from tokens import app_key, app_secret, access_token, refresh_token
-from settings import scopes, user_agent
+from bot.tokens import app_key, app_secret, access_token, refresh_token
+from bot.settings import scopes, user_agent
 
 reddit_client = praw.Reddit(user_agent=user_agent)
 oauth_helper = PrawOAuth2Mini(reddit_client, app_key=app_key,
                               app_secret=app_secret, access_token=access_token,
                               scopes=scopes, refresh_token=refresh_token)
 
+def parse_and_resolve_requests(comment_id, requests):
+    requests = requests.split(" ")
+    for request in requests:
+        print("request = ", request)
+
+
+
 def run_bot():
     oauth_helper.refresh()
     for comment in reddit_client.get_comments('test'):
         if comment.body.lower().startswith('ci_rae '):
             print('Found request')
-            comment.reply('Hello!')
+            parse_and_resolve_requests(comment, comment.body.lower())
 
 while True:
     try:
@@ -32,4 +38,7 @@ while True:
         print(rle.error_type, rle.message)
         print("Sleeping for ", rle.sleep_time, " seconds")
         time.sleep(rle.sleep_time)
+
+
+
 
