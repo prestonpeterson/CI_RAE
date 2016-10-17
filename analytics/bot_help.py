6c7866d489cd7a4566144926f3818fe99668cab9
Zoltan Batoczki <zoltanbatoczki@gmail.com>
2016-10-16 17:49:12 -0700

    updated message

--- analytics/bot_help.py
+++ analytics/bot_help.py
@@ -5,11 +5,10 @@ class CIRAEHelp:
         self.comment = comment #comment object
     def ci_rae_help(self):
         user_name = (self.comment).author.name
-        answer = ("Thanks for using CI_RAE, "+ user_name + "! Follwing the format \"ci_rae <command>\", here is a list of commands that you can use:\n\n"
+        answer = ("Thanks for using CI_RAE, "+ user_name + "! Follwing the format \"/u/ci_rae <command>\", here is a list of commands that you can use:\n\n"
         "**user_activity**: generates a graph that shows when a user is most active\n\n"
         "**word_cloud**: generates a word cloud based on your comment history\n\n"
         "**word_count**: generates a graph that shows the words you used the most in your comment history\n\n"
-        "**karma_breakdown**: generates a graph that shows your karma based on which subreddits you've submitted and commented on\n\n"
-        "**best_worst**: shows you your best and worst comments and submission")
+        "**karma_breakdown**: generates a graph that shows your karma based on which subreddits you've submitted and commented on")
         (self.comment).reply(answer)
         print("Responded to command")
