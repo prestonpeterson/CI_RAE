643c7bdc9701e3414ca9195976d5fe89d05a8e52
gitgiant <prezidentbush@gmail.com>
2016-10-17 12:12:03 -0700

    Add files via upload
    removed nltk (not needed), made the image cloud bigger (using scale) made the the cloud randomly generated (random_state=1)

--- analytics/image_cloud.py
+++ analytics/image_cloud.py
@@ -1,47 +1,44 @@
-#to install wordcloud use command:
-# pip3 install wordcloud
-#to install natural language tool kit use command:
-# pip3 install nltk
-#then you need to download the corpus, fire up a python terminal
-#>>> import nltk
-#>>> nltk.download()
-
 from PIL import Image
 from os import path
 import praw
 import numpy as np
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
-from nltk.corpus import stopwords
 
-#stopwords from wordcloud
-s = set(STOPWORDS)
+class CIImageCloud:
+    def __init__(self,user_name):
+        self.user_name = user_name
+    def image_cloud(self):
 
-# Per the reddit API, user agent should follow format: <platform>:<app ID>:<version string> (by /u/<reddit username>)
-user_agent = ("Ubuntu 16.04:CI-RAE:V0.1 (by /u/giantmatt)")
+        #stopwords from wordcloud
+        s = set(STOPWORDS)
+        s.add("one")
 
-r = praw.Reddit(user_agent=user_agent)
+        # Per the reddit API, user agent should follow format: <platform>:<app ID>:<version string> (by /u/<reddit username>)
+        user_agent = ("Ubuntu 16.04:CI-RAE:V0.1 (by /u/giantmatt)")
 
-#set to grab a certain number of things from reddit...reddit wont return more than 1000
-thing_limit = 1000
-user_name = input("Please Enter a user to get their top 20 most used words:\n")
+        r = praw.Reddit(user_agent=user_agent)
 
-user = r.get_redditor(user_name)
+        #set to grab a certain number of things from reddit...reddit wont return more than 1000
+        thing_limit = 1000
+        #user_name = input("Please Enter a user to get their top 20 most used words:\n")
 
-generated = user.get_comments(time='all', limit=None)
-reddit_mask = np.array(Image.open(path.join("./", "reddit.png")))
+        user = r.get_redditor(self.user_name)
 
-total_comments = str()
-for thing in generated:
-    total_comments += thing.body
-    total_comments += " "
-#filter out common stopwords using the nltk stopwords corpus
-filtered_comments = ' '.join([word for word in total_comments.split() if word not in (stopwords.words('english'))])
+        generated = user.get_comments(time='all', limit=None)
+        reddit_mask = np.arrayImage.open(path.join("./", "reddit.png"))
 
-# take relative word frequencies into account, lower max_font_size
-wordcloud = WordCloud(max_font_size=100, mask=reddit_mask, relative_scaling=1, stopwords=s).generate(filtered_comments)
-plt.figure()
-plt.imshow(wordcloud)
-plt.axis("off")
-plt.show()
-plt.savefig(user_name+"_imagecloud.png")
+        total_comments = str()
+        for thing in generated:
+            total_comments += thing.body
+            total_comments += " "
+
+        # take relative word frequencies into account, lower max_font_size
+        wordcloud = WordCloud(scale=3, mask=reddit_mask, relative_scaling=.5, random_state=1, stopwords=s).generate(total_comments)
+        plt.figure()
+        plt.title(self.user_name + "'s image cloud")
+        plt.imshow(wordcloud)
+        plt.axis("off")
+        plt.show()
+        #plt.savefig(self.user_name+"_imagecloud.png")
+        Image.close(path.join("./", "reddit.png"))
