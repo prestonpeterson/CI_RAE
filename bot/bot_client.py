5a12ec19a4e3a4c0f77a9bd356f08611cd26c1bc
byteoffset <ptown77@gmail.com>
2016-10-23 20:02:47 -0700

    Cleaned up and commented all the code in bot_client.py. Should be way more readable now

--- bot/bot_client.py
+++ bot/bot_client.py
@@ -7,87 +7,111 @@ from bot.tokens import app_key, app_secret, access_token, refresh_token
 from bot.settings import scopes, user_agent, subreddits, SLEEP_TIME, user_name, user_pass
 from analytics import request_handler
 
-cache = set() # comment ids can/should be removed from this set once they have been replied to
-sub_wait = {} # rate limit wait times for subreddits
-current_sub = ''
-
-reddit_client = praw.Reddit(user_agent=user_agent)
-oauth_helper = PrawOAuth2Mini(reddit_client, app_key=app_key,
-                              app_secret=app_secret, access_token=access_token,
-                              scopes=scopes, refresh_token=refresh_token)
-reddit_client.login(user_name, user_pass, disable_warning=True)
 
 
+class BotClient:
+    def __init__(self):
+        print("Starting up the reddit bot...")
+        print("Authenticating to Reddit...")
+        self.reddit_client = praw.Reddit(user_agent=user_agent)
+        self.oauth_helper = PrawOAuth2Mini(self.reddit_client, app_key=app_key,
+                                      app_secret=app_secret, access_token=access_token,
+                                      scopes=scopes, refresh_token=refresh_token)
+        self.reddit_client.login(user_name, user_pass, disable_warning=True)
+        print("Authentication successful!")
 
-def check_sub_wait(subreddit, subs):
-    s = subreddit.display_name
-    if s in subs:
+        self.cache = set() # comment ids can/should be removed from this set once they have been replied to
+        self.subreddit_wait_times = {} # rate limit wait times for subreddits
+        self.current_sub = ''
+        print("Initiating the Listening process")
+
+        # infinite loop to run the listener in
+        while True:
+            try:
+                self.listen_for_mentions()
+            except praw.errors.OAuthInvalidToken:
+                # access tokens expire hourly, so must be periodically refreshed
+                self.oauth_helper.refresh()
+            except praw.errors.RateLimitExceeded as rle:
+                # Reddit limits number of comments you can make per minute
+                # depending on factors like age and karma score of the bot's reddit account
+                # If bot tries to comment too frequently, this exception will be caught
+                print(rle.error_type, rle.message)
+                print("Sleeping for ", rle.sleep_time, " seconds")
+                self.add_subreddit_wait_time(rle.sleep_time, self.current_sub, self.subreddit_wait_times)
+                time.sleep(rle.sleep_time)
+            except Exception as e:
+                print('Error: ', e)
+
+    def check_sub_wait(self, subreddit, subs):
+        """
+        checks if the given subreddit is in the dictionary of subs that the bot is timed out from
+        :param subreddit: subreddit to check for existence in the dictionary
+        :param subs: the list of subs that the bot is currently timed out from
+        :return: True if the bot is currently timed out from the given subreddit. False otherwise
+        """
+        s = subreddit.display_name
+        if s in subs:
+            now = time.time()
+            wait_time = subs[s]
+            if now >= wait_time:
+                # bot is no longer timed out from given subreddit. remove this subreddit from the subs dictionary
+                del subs[s]
+            else:
+                # bot is currently timed out from the given subreddit
+                return True
+        return False
+
+    def add_subreddit_wait_time(self, subreddit_timeout, subreddit, subs):
+        """
+        Add current subreddit timeout to subreddit_wait_times list.
+        :param subreddit_timeout: number of seconds til bot can post to this subreddit
+        :param subreddit: the subreddit that the bot is currently timed out from
+        :param subs: the list of subreddits that the bot is timed out from
+        """
         now = time.time()
-        wait_time = subs[s]
-        print('%i < %i?' % (now, wait_time))
-        if now >= wait_time:
-            del subs[s]
-        else:
-            return True
-    return False
+        s = subreddit.display_name
+        subs[s] = now + subreddit_timeout
 
-def add_sub_wait(sleep_time, subreddit, subs):
-    now = time.time()
-    s = subreddit.display_name
-    subs[s] = now+sleep_time
+    def listen_for_mentions(self):
+        """
+        Refresh reddit authentication token, check for new Reddit Mentions of /u/ci_rae.
+        If new mentions are found, mark them as read hand them over to the request handler class.
+        """
+        self.oauth_helper.refresh()
+        mentions = self.reddit_client.get_mentions()
 
+        # Check new mentions
+        for m in mentions:
+            current_sub = m.subreddit
 
-def run_bot():
-    oauth_helper.refresh()
-    mentions = reddit_client.get_mentions()
+            if m.id in self.cache:
+                continue
+            if m.subject != 'username mention':
+                m.mark_as_read()
+                continue
 
-    # Check new mentions
-    for m in mentions:
-        current_sub = m.subreddit
+            if self.check_sub_wait(m.subreddit, self.subreddit_wait_times):
+                continue
 
-        if m.id in cache:
-            continue
-        if m.subject != 'username mention':
-            m.mark_as_read()
-            continue
+            for reply in m.replies:
+                if reply.author.name == 'ci_rae':
+                    self.cache.add(m.id)
 
-        if check_sub_wait(m.subreddit, sub_wait):
-            continue
+            # Process the message
+            if m.id not in self.cache:
+                # TODO: Call the (threaded) Request Handler class to process the user's requests
+                print("Found request")
+                m.reply(
+                    "yao yao")  # placeholder reply message. probably delete this line after adding the call to the request handler class
+                m.mark_as_read()
 
+            self.cache.add(m.id)
 
-
-        for reply in m.replies:
-            if reply.author.name == 'ci_rae':
-                cache.add(m.id)
-
-        # Process the message
-        if m.id not in cache:
-            # Call Phillip's Request Handler class to process the user's request(s)
-            print("Found request")
-            m.reply("yao yao")
-            m.mark_as_read()
-
-        cache.add(m.id)
-
-    time.sleep(SLEEP_TIME)
-
-while True:
-    try:
-        run_bot()
-    except praw.errors.OAuthInvalidToken:
-        # access tokens expire hourly, so must be periodically refreshed
-        oauth_helper.refresh()
-    except praw.errors.RateLimitExceeded as rle:
-        # Reddit limits number of comments you can make per minute
-        # depending on factors like age of your account and karma score
-        # If bot tries to comment too frequently, this exception will be caught
-        print(rle.error_type, rle.message)
-        print("Sleeping for ", rle.sleep_time, " seconds")
-        add_sub_wait(rle.sleep_time.sleep_time, current_sub, sub_wait)
-        time.sleep(rle.sleep_time)
-    except Exception as e:
-        print('Error: ', e)
+        time.sleep(SLEEP_TIME)
 
 
+if __name__ == "__main__":
+    BotClient()
 
 
