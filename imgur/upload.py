eb83f48a530c8c92e37bce5b1319ab0b288acaa3
byteoffset <ptown77@gmail.com>
2016-10-23 18:59:37 -0700

    Added exception handling to upload.py and added creation of the imgur_client object to the beginning of the file

--- imgur/upload.py
+++ imgur/upload.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python3
 
+from imgur.imgur_settings import client_id, client_secret
+from imgurpython import ImgurClient
+imgur_client = ImgurClient(client_id, client_secret)
 
-def upload_image(imgur_client, image_path):
+def upload_image(image_path):
     """
-    :param client: the path to the image to be uploaded. e.g.
-    :return: string URL of the uploaded image
+    :param client: the relative path to the image to be uploaded. e.g. 'test.png'
+    :return: string URL of the uploaded image.
+             or return "UPLOAD_ERROR" if an exception occurs, such as the imgur servers being busy.
     """
     print("Uploading image... ")
     try:
         image = imgur_client.upload_from_path(image_path, config=None, anon=True)
     except Exception as e:
-        print('Error: ', e)
-    print("Done")
+        print('Error uploading image: ', e)
+        return "UPLOAD_ERROR"
+    print("Done uploading")
     return format(image['link'])
 
 
 # The following is for testing of imgur uploads
 if __name__ == "__main__":
-    # Any python file wishing to call the upload_image function must include the following imports:
-    from imgur.imgur_settings import client_id, client_secret
-    from imgurpython import ImgurClient
-
-    imgur_client = ImgurClient(client_id, client_secret)
     image_path = 'test.png'
-    image_url = upload_image(imgur_client, image_path)
-
+    image_url = upload_image(image_path)
     print("Image posted. Here is the link: ", image_url)
