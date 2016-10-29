624d1c99dbd9be09c47e924fcdb805ab316b3135
Zoltan Batoczki <zoltanbatoczki@gmail.com>
2016-10-29 13:44:40 -0700

    Removed class; added documentation
    Parameters include reddit_user to grab list of submissions and comments.
    comment parameter used to reply back to reddit user

--- analytics/best_worst.py
+++ analytics/best_worst.py
@@ -1,40 +1,39 @@
 import praw
 
-class BestWorst:
-    def __init__(self, comment):
-        self.user_name = comment.author.name #user name
-        self.comment = comment
-    def best_worst(self, reddit_client):
-        comment_block = [] #list to prepare reply
-        account = reddit_client.get_redditor(self.user_name)
+def best_worst(comment,reddit_user):
+    """@param comment A reddit Comment object to reply to.
+       @param reddit_user A reddit user object
+    """
+    comment_block = [] #list to prepare reply
+    #account = reddit_client.get_redditor(self.user_name)
 
-        #get top comment
-        top_comment = account.get_comments(limit=1, sort='top')
-        for comment in top_comment:
-            top_score = str(comment.score)
-            comment_block.append("## BEST COMMENT\n\n>"+comment.body+"    \n\nScore: **"+ top_score +"**")
+    #get top comment
+    top_comment = reddit_user.get_comments(limit=1, sort='top')
+    for comment in top_comment:
+        top_score = str(comment.score)
+        comment_block.append("## BEST COMMENT\n\n>"+comment.body+"    \n\nScore: **"+ top_score +"**")
 
-        #get worst comment
-        worst_comment = account.get_comments(limit=1, sort='controversial')
-        for bad_comment in worst_comment:
-            worst_score = str(bad_comment.score)
-            comment_block.append("## WORST COMMENT\n\n>"+bad_comment.body+"    \n\nScore: **"+ worst_score +"**")
+    #get worst comment
+    worst_comment = reddit_user.get_comments(limit=1, sort='controversial')
+    for bad_comment in worst_comment:
+        worst_score = str(bad_comment.score)
+        comment_block.append("## WORST COMMENT\n\n>"+bad_comment.body+"    \n\nScore: **"+ worst_score +"**")
 
-        #get top submission
-        top_submission = account.get_submitted(limit=1, sort='top')
-        for submission in top_submission:
-            link = submission.permalink
-            submission_score = str(submission.score)
-            comment_block.append("Your [top submission]("+ link + ") has a score of **"+ submission_score +"**")
+    #get top submission
+    top_submission = reddit_user.get_submitted(limit=1, sort='top')
+    for submission in top_submission:
+        link = submission.permalink
+        submission_score = str(submission.score)
+        comment_block.append("Your [top submission]("+ link + ") has a score of **"+ submission_score +"**")
 
-        #get worst submission
-        worst_submission = account.get_submitted(limit=1, sort='controversial')
-        for submission in worst_submission:
-            link = submission.permalink
-            submission_score = str(submission.score)
-            comment_block.append("Your [worst submission]("+ link + ") has a score of **"+ submission_score +"**")
+    #get worst submission
+    worst_submission = reddit_user.get_submitted(limit=1, sort='controversial')
+    for submission in worst_submission:
+        link = submission.permalink
+        submission_score = str(submission.score)
+        comment_block.append("Your [worst submission]("+ link + ") has a score of **"+ submission_score +"**")
 
-        answer = "\n\n***\n\n".join(comment_block)
-        answer = "# BEST & WORST\n\n***\n\n" + answer
-        self.comment.reply(answer)
-        print("Comment has been replied")
+    answer = "\n\n***\n\n".join(comment_block)
+    answer = "# BEST & WORST\n\n***\n\n" + answer
+    comment.reply(answer)
+    print("'best_worst() function successfully called'") #debug purposes
