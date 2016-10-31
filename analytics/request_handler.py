af069aa46e886eed8a7de695d812bb61176d3381
gitgiant <prezidentbush@gmail.com>
2016-10-31 13:02:07 -0700

    added a try block
    added a try block to catch user not found error or any other errors, and will mark the comment as read to keep the bot from trying to respond to the same buggy request and crashing over and over

--- analytics/request_handler.py
+++ analytics/request_handler.py
@@ -6,6 +6,7 @@ from analytics import user_activity
 from analytics import word_cloud
 from analytics import word_count
 from analytics import best_worst
+import praw
 import threading
 
 class RequestThread(threading.Thread):
@@ -18,28 +19,31 @@ class RequestThread(threading.Thread):
         print("requester = ", self.comment.author)
         if len(requests) >= 2:
             print("command = ", requests[1])
+        try:
+            if len(requests) <= 2:
+                redditor_object = self.reddit_client.get_redditor(self.comment.author)
+            else:
+                redditor_object = self.reddit_client.get_redditor(requests[2])
+            if len(requests) <= 1:
+                reply = bot_help.ci_rae_help(self.comment)
+            elif requests[1] == 'best_worst':
+                reply = best_worst.best_worst(self.comment, redditor_object)
+            elif requests[1] == 'image_cloud':
+                reply = image_cloud.image_cloud(redditor_object)
+            elif requests[1] == 'karma_breakdown':
+                reply = karma_breakdown.karma_breakdown(redditor_object)
+            elif requests[1] == 'user_activity':
+                reply = user_activity.user_activity(redditor_object)
+            elif requests[1] == 'word_cloud':
+                reply = word_cloud.word_cloud(redditor_object)
+            elif requests[1] == 'word_count':
+                reply = word_count.word_count(redditor_object)
+            else:
+                reply = bot_help.ci_rae_help(self.comment)
+            self.comment.reply(reply)
+            print('Reply sent.')
+        except:
+            print('error, marking mention as read')
+            self.comment.mark_as_read()
 
-        if len(requests) <= 2:
-            redditor_object = self.reddit_client.get_redditor(self.comment.author)
-        else:
-            redditor_object = self.reddit_client.get_redditor(requests[2])
-
-        if len(requests) <= 1:
-            reply = bot_help.ci_rae_help(self.comment)
-        elif requests[1] == 'best_worst':
-            reply = best_worst.best_worst(self.comment, redditor_object)
-        elif requests[1] == 'image_cloud':
-            reply = image_cloud.image_cloud(redditor_object)
-        elif requests[1] == 'karma_breakdown':
-            reply = karma_breakdown.karma_breakdown(redditor_object)
-        elif requests[1] == 'user_activity':
-            reply = user_activity.user_activity(redditor_object)
-        elif requests[1] == 'word_cloud':
-            reply = word_cloud.word_cloud(redditor_object)
-        elif requests[1] == 'word_count':
-            reply = word_count.word_count(redditor_object)
-        else:
-            reply = bot_help.ci_rae_help(self.comment)
-        self.comment.reply(reply)
-        print('Reply sent.')
 
