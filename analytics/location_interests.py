b9eec2381bb2b6ba51c325ff2238bd1ac55386a1
Zoltan Batoczki <zoltanbatoczki@gmail.com>
2016-11-05 18:24:48 -0700

    Updated reply variable to work with returning value to debug and handler

--- analytics/location_interests.py
+++ analytics/location_interests.py
@@ -3,14 +3,12 @@ from locations import *
 
 """@package docstring
 Allows the user to recieve a list of locations they have recently (up to 1000 items) mentioned.
-Locations now only include countries and U.S. states.
+Locations now only include countries and U.S. states which are grabbed from locations.py.
 """
 
 found_locations = []
 """@var reply A formatted string used to reply back to user.
 """
-reply = "## Based on your recent activity, here are some places you mentioned.\n\n***\n\n"
-
 def lookfor_locations(word):
     if word not in found_locations and (word in US_States or word in countries):
         found_locations.append(word)
@@ -42,16 +40,15 @@ def location_interests(reddit_user):
             word = re.sub(r'[^a-zA-Z\s]', '', word) #regex removes any special characters and digits
             lookfor_locations(word)
 
+    reply = "## Based on your recent activity, here are some places you mentioned.\n\n***\n\n"
     for location in found_locations:
-        global reply
         reply += "+ [{0}](https://www.google.com/maps/place/{0})    ".format(location)
-    #print(reply)
     return reply
 
 if __name__ == '__main__':
     import praw
     #from bot.settings import user_agent
-    client = praw.Reddit(user_agent="script testing by /u/Zoltt93")
+    client = praw.Reddit(user_agent="Reddit Analytics Engine, written by CSUCI students")
     reddit_u = client.get_redditor('giantmatt')
-    location_interests(reddit_u)
-    print("DONE")
+    answer = location_interests(reddit_u)
+    print(answer)
