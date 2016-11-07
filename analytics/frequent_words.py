d1fb51df3b8aea4836ac7419f301da5715cc88f5
Jesus805 <jesus.805@hotmail.com>
2016-11-06 21:23:41 -0800

    Added easy categories to subreddit analytics
    Refer to the dictionary for valid choices

--- analytics/frequent_words.py
+++ analytics/frequent_words.py
@@ -6,20 +6,50 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+# Syntax:
+# subreddit_get['hot']['none']
+# subreddit_get['top']['today'] etc.
+subreddit_get = {
+            'hot': {'none': 'get_hot'},
+            'rising': {'none': 'get_rising'},
+            'new': {'none': 'get_new'},
+            'controversial': {
+                  'none': 'get_controversial',
+                   'all': 'get_controversial_from_all',
+                 'today': 'get_controversial_from_day',
+             'past_hour': 'get_controversial_from_hour',
+            'past_month': 'get_controversial_from_month',
+             'past_week': 'get_controversial_from_week',
+             'past_year': 'get_controversial_from_year'},
+
+            'top': {
+                  'none': 'get_top',
+                   'all': 'get_top_from_all',
+                 'today': 'get_top_from_day',
+             'past_hour': 'get_top_from_hour',
+            'past_month': 'get_top_from_month',
+             'past_week': 'get_top_from_week',
+             'past_year': 'get_top_from_year'}
+            }
 
 
-def subreddit_current_trend(subreddit, save_path='', submission_limit=5, debug=False, skip_more_comments=True):
+def frequent_words(subreddit, category='hot', time='none', save_path='', submission_limit=5, debug=False, skip_more_comments=True):
     # A dictionary containing all the words in the comments
     # Key (String): the word used
     # Value (int): count of how many times the word was said
     counted_comments = {}
     # using stopwords from wordcloud to remove insignificant words ('The', 'at', etc.)
     stop_words = set(STOPWORDS)
+    # TODO: Add better STOPWORD detection (some symbols elude the filter)
     stop_words.add('*')
     stop_words.add('i')
     stop_words.add('>')
+
     # Get submissions from subreddit
-    for submission in subreddit.get_hot(limit=submission_limit):
+    get = getattr(subreddit, subreddit_get[category][time])
+    all_submissions = get(limit=submission_limit)
+
+    for submission in all_submissions:
         # A list containing all the words in the comment
         words_in_comment = []
 
@@ -36,10 +66,11 @@ def subreddit_current_trend(subreddit, save_path='', submission_limit=5, debug=F
             else:
                 if skip_more_comments:
                     continue
-                # TODO: Impelement submission.replace_more_comments, this requires that you adhere to the API guidelines
+                # TODO: Implement submission.replace_more_comments, but this requires that you adhere to the Reddit API guidelines
         # Filter out stopwords
         words_in_comment = filter(lambda word: word not in stop_words, map(str.lower, words_in_comment))
         # Count words in words_in_comment and generate a dictionary <string Word, int Count>
+        # TODO: Implement more efficient grouping algorithm | Example: crash, crashing, and crashes should all be one word
         counted_comments.update(Counter(words_in_comment))
 
     # Remove entries that have a value of 0
@@ -48,10 +79,9 @@ def subreddit_current_trend(subreddit, save_path='', submission_limit=5, debug=F
     sorted_x = sorted(counted_comments.items(), key=itemgetter(1), reverse=True)
     top20 = sorted_x[:20]
 
-    # Get just the keys into a sorted list, and trim to get the top 20
+    # Seperate keys and values
     sorted_keys = list(map(itemgetter(0), top20))
 
-    # Get just the keys into a sorted values, and trim to get the top 20
     sorted_values = list(map(itemgetter(1), top20))
 
     max_y_tick = np.amax(sorted_values) + 1
@@ -61,7 +91,7 @@ def subreddit_current_trend(subreddit, save_path='', submission_limit=5, debug=F
     fig, ax = plt.subplots()
     ax.bar(sorted_range, sorted_values, bar_width, align='center')
 
-    ax.set_title("Top 20 most common words in /r/" + subreddit.display_name)
+    ax.set_title("Top 20 most common words in /r/" + subreddit.display_name + ' category: ' + category)
 
     ax.set_xlabel("Word")
     ax.set_ylabel("Frequency", rotation='vertical')
@@ -104,4 +134,5 @@ if __name__ == '__main__':
     reddit = praw.Reddit(user_agent)
     reddit.login(username='ci_rae', password='herpderp')
     subreddit = reddit.get_subreddit('pokemongo')  # pokemongo, LUL
-    subreddit_current_trend(subreddit, debug=True)
+    # Refer to the dictionary to see which category and time choices you can make
+    frequent_words(subreddit, category='top', time='today', debug=True)
