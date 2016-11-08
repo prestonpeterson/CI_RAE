35c5fa843a0a5a45951c34f0c920e94796f02080
byteoffset <ptown77@gmail.com>
2016-11-08 11:11:54 -0800

    #15: Lots of fixes and improvements to product search

--- analytics/sentiment_search.py
+++ analytics/sentiment_search.py
@@ -1,13 +1,12 @@
 import nltk
 import yaml
 import praw
-import os
-from math import trunc
 
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 
 
+# Class Splitter derived from http://fjavieralba.com/basic-sentiment-analysis-with-python.html
 class Splitter(object):
     def __init__(self):
         self.nltk_splitter = nltk.data.load('tokenizers/punkt/english.pickle')
@@ -24,6 +23,7 @@ class Splitter(object):
         return tokenized_sentences
 
 
+# Clas POSTagger derived from http://fjavieralba.com/basic-sentiment-analysis-with-python.html
 class POSTagger(object):
     def __init__(self):
         pass
@@ -44,23 +44,23 @@ class POSTagger(object):
         return pos
 
 
-# class DictionaryTagger from http://fjavieralba.com/basic-sentiment-analysis-with-python.html
+# Class DictionaryTagger derived from http://fjavieralba.com/basic-sentiment-analysis-with-python.html
 class DictionaryTagger(object):
     def __init__(self, dictionary_paths):
         files = [open(path, 'r') for path in dictionary_paths]
         dictionaries = [yaml.load(dict_file) for dict_file in files]
         for file in files:
             file.close()
-        # map(lambda x: x.close(), files)
         self.dictionary = {}
         self.max_key_size = 0
         for curr_dict in dictionaries:
-            for key in curr_dict:
-                if key in self.dictionary:
-                    self.dictionary[key].extend(curr_dict[key])
-                else:
-                    self.dictionary[key] = curr_dict[key]
-                    self.max_key_size = max(self.max_key_size, len(key))
+            if curr_dict is not None:
+                for key in curr_dict:
+                    if key in self.dictionary:
+                        self.dictionary[key].extend(curr_dict[key])
+                    else:
+                        self.dictionary[key] = curr_dict[key]
+                        self.max_key_size = max(self.max_key_size, len(key))
 
     def tag(self, postagged_sentences):
         return [self.tag_sentence(sentence) for sentence in postagged_sentences]
@@ -137,12 +137,16 @@ def calc_sentiment_score(review):
     return sum([sentence_score(sentence, None, 0.0) for sentence in review])
 
 
-def check_sentiment(splitter, postagger, dicttagger, comment, debug=False):
-    splitted_sentences = splitter.split(comment.body.lower())
+def check_sentiment(splitter, postagger, dicttagger, comment, search_terms, debug=False):
+    terms = search_terms.lower().split(' ')
+    sentences = comment.body.lower()
+    for term in terms:
+        sentences = sentences.replace(term, '')
+    splitted_sentences = splitter.split(sentences)
     pos_tagged_sentences = postagger.pos_tag(splitted_sentences)
     dict_tagged_sentences = dicttagger.tag(pos_tagged_sentences)
     score = calc_sentiment_score(dict_tagged_sentences)
-    adjusted_score = score * (comment.score / 2)
+    adjusted_score = (score/2) * comment.score
     if debug:
         print('comment: ', comment.body)
         print('\tupvotes: ', comment.score)
@@ -151,71 +155,81 @@ def check_sentiment(splitter, postagger, dicttagger, comment, debug=False):
     return adjusted_score
 
 
-def sentiment_search(agent, keyword, debug=True):
+def sentiment_search(agent, search_term, debug=False):
     # TODO: Add functionality for '--thorough' argument which increases thread limit and more_comments limit
     # TODO: Add mutex for this function
-    keyword = keyword.lower()
     splitter = Splitter()
     postagger = POSTagger()
-    thread_limit = 12
-    more_comments_limit = 5
-    more_comments_threshold = 2
+    thread_limit = 50
     dicttagger = DictionaryTagger(
         ['../analytics/sentiment_dicts/positive.yml', '../analytics/sentiment_dicts/negative.yml',
          '../analytics/sentiment_dicts/inc.yml', '../analytics/sentiment_dicts/dec.yml',
          '../analytics/sentiment_dicts/inv.yml']
     )
-    search = agent.search(keyword, limit=thread_limit, sort='top', subreddit='all', period='month')
-    sentiment_score = int(0)
+    search = agent.search(search_term, limit=thread_limit, sort='relevance', subreddit='all', period='month')
+    sentiment_score = 0
+    total_sentiment = 0
     for index, result in enumerate(search):
         print('***COMPUTING SENTIMENT SCORE.... {:2.0f}% COMPLETE***'.format((float(index / thread_limit) * 100)))
         try:
-            result.replace_more_comments(limit=more_comments_limit, threshold=more_comments_threshold)
+            if abs(sentiment_score) <= 20:
+                # expand search
+                more_comments_limit = 15
+                more_comments_threshold = 0
+            else:
+                more_comments_limit = 5
+                more_comments_threshold = 1
+            # result.replace_more_comments(limit=more_comments_limit, threshold=more_comments_threshold)
+            flattened_comments = praw.helpers.flatten_tree(result.comments)
             if result.comments is not None:
-                for comment in result.comments:
-                    if type(comment) is praw.objects.Comment and keyword in comment.body.lower() and comment.score > 0:
-                        sentiment_score += int(check_sentiment(splitter, postagger, dicttagger, comment, debug=debug))
+                for comm in flattened_comments:
+                    if type(comm) is praw.objects.Comment and search_term.lower() in comm.body.lower() and comm.score > 0:
+                        result = check_sentiment(splitter, postagger, dicttagger, comm, search_term, debug=debug)
+                        sentiment_score += result
+                        total_sentiment += abs(result)
         except Exception as e:
             print(e)
 
     if debug:
-        print('*********\nFINAL SENTIMENT SCORE FOR KEYWORD "{}" = {}\n*********'.format(keyword, str(sentiment_score)))
-    result_str = 'Sentiment Score ' + str(sentiment_score) + ': '
-    if sentiment_score > 9000:  # this probably won't happen much
-        result_str += '**IT\'S OVER 9000** '
-    if sentiment_score >= 2000:
-        result_str += '_**Overwhelmingly Positive**_'
-    elif sentiment_score >= 1500:
-        result_str += '**Extremely Positive**'
-    elif sentiment_score >= 1000:
-        result_str += '**Very Positive**'
-    elif sentiment_score >= 500:
-        result_str += '**Fairly Positive**'
-    elif sentiment_score >= 200:
-        result_str += '**Slightly Positive**'
-    elif sentiment_score <= -2000:
-        result_str += '_**Overwhelmingly Negative**_'
-    elif sentiment_score <= -1500:
-        result_str += '**Extremely Negative**'
-    elif sentiment_score <= -1000:
-        result_str += '**Very Negative**'
-    elif sentiment_score <= -500:
-        result_str += '**Fairly Negative**'
-    elif sentiment_score <= -200:
-        result_str += '**Slightly Negative**'
-    else:  # 200 > sentiment_score > -200
-        result_str += '**Neutral**'
-    result_str += '*\n\n&nbsp;\n^(*^Based ^on ^Reddit ^users ^usage ^of ^search ^term ^{} ^over ^the ^past ^30 ^days)'.format(keyword)
+        print('*********\nFINAL SENTIMENT SCORE FOR KEYWORD "{}" = {}\n*********'.format(search_term, str(sentiment_score)))
+        print('*********\nTOTAL SENTIMENT = {}\n**********'.format(total_sentiment))
+    sentiment_percent = int(float(sentiment_score / total_sentiment) * 100)
+    result_str = '### Sentiment Score ' + str(int(sentiment_percent)) + ': '
+    if sentiment_percent >= 65:
+        result_str += '_Overwhelmingly Positive_'
+    elif sentiment_percent >= 50:
+        result_str += '_Extremely Positive_'
+    elif sentiment_percent >= 35:
+        result_str += '_Mostly Positive_'
+    elif sentiment_percent >= 15:
+        result_str += '_Fairly Positive_'
+    elif sentiment_percent >= 5:
+        result_str += '_Slightly Positive_'
+    elif sentiment_percent <= -65:
+        result_str += '_Overwhelmingly Negative_'
+    elif sentiment_percent <= -50:
+        result_str += '_Extremely Negative_'
+    elif sentiment_percent <= -35:
+        result_str += '_Mostly Negative_'
+    elif sentiment_percent <= -15:
+        result_str += '_Fairly Negative_'
+    elif sentiment_percent <= -5:
+        result_str += '_Slightly Negative_'
+    else:
+        result_str += 'Neutral**'
+    search_term = search_term.split(' ')
+    result_str += '*\n***\n&nbsp;\n^(*^Based ^on ^Reddit ^users ^usage ^of ^search ^term ^`{}` ^over ^the ^past ^30 ^days)'.format('` ^`'.join(search_term))
     return result_str
 
 
 if __name__ == '__main__':
     from bot.settings import user_agent
-
     client = praw.Reddit(user_agent)
+
     # print(sentiment_search(client, 'macbook', debug=True))
     # print(sentiment_search(client, 'skyrim', debug=True))
-    print(sentiment_search(client, 'galaxy note 7', debug=True))
-    # print(sentiment_search(client, 'nickelback', debug=True))
+    # print(sentiment_search(client, 'galaxy note 7', debug=True))
+    # print(sentiment_search(client, 'game of thrones', debug=True))
+    print(sentiment_search(client, 'breaking bad', debug=True))
     # print(sentiment_search(client, 'game of thrones', debug=True))
     # print(sentiment_search(client, 'a;dslfkjz;ldfkjzx;lfkjaszd;flkj', debug=True))
