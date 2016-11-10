ba22ec58f8edd3b3e09bd1402b831f7ec34dba55
Preston Peterson <ptown77@gmail.com>
2016-11-09 20:58:11 -0800

    Update tokens.py
    Removed the bot app_key and app_secret from the repo. Keep these on your local machinez

--- bot/tokens.py
+++ bot/tokens.py
@@ -1,6 +1,6 @@
 # bot app credentials and oauth2 tokens for Reddit. keep it secret, keep it safe
 
-app_key = '2DojNHAzx-I5gQ'
-app_secret = 'UvcLOauVTTGiKDVaidchDDatYz8'
+app_key = '' # log in to the bot account on Reddit and go to https://ssl.reddit.com/prefs/apps if you need get the key
+app_secret = '' # log in to the bot account on Reddit and go to https://ssl.reddit.com/prefs/apps if you need to get the secret
 access_token = 'IMeh7iB7Q2knJlr764sTKuKThO4'
 refresh_token = '63392154-0BGTjFuDptyCwwQD9PM754zBj_E'
