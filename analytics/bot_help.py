0b06f8848ea9fd3fdf43cc304d7f6bc2a0e6434a
Peterson <preston.peterson651@sie-1222-22.csuci.edu>
2016-11-14 12:18:43 -0800

    Added sentiment_search to bot help text

--- analytics/bot_help.py
+++ analytics/bot_help.py
@@ -8,12 +8,13 @@ def ci_rae_help(redditor):
     user_name = redditor.name
     answer = ("Thanks for using CI_RAE, "+ user_name + "! Follwing the format \"/u/ci_rae <command> <target>\", here is a list of commands that you can use:\n\n"
     "**user_activity**: Generates a graph that shows when a user is most active\n\n"
-    "**word_cloud**: Generates a word cloud based on your comment history\n\n"
-    "**word_count**: Generates a graph that shows the words you used the most in your comment history\n\n"
-    "**karma_breakdown**: Generates a graph that shows your karma based on which subreddits you've submitted and commented on\n\n"
+    "**word_cloud**: Generates a word cloud based on your comment history.\n\n"
+    "**word_count**: Generates a graph that shows the words you used the most in your comment history.\n\n"
+    "**karma_breakdown**: Generates a graph that shows your karma based on which subreddits you've submitted and commented on.\n\n"
     "**image_cloud**: Generates a word cloud shaped as Snoo, reddit's mascot.\n\n"
     "**locations**: Generates a list of locations you have mentioned such as countries and states (currently, only U.S. states are listed).\n\n"
-    "**user_interests**: generates a graph that shows what interests the user the most")
+    "**user_interests**: Generates a graph that shows what interests the user the most.\n\n"
+    "**sentiment_search**: Scans Reddit user's usage of given target product or terms, performs a linguistic analysis to determine Reddit's overall sentiment on the subject: good, bad, or otherwise.")
     return answer
 
 if __name__ == '__main__':
