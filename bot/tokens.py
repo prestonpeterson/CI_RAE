5422c7de6aced964eff0ee0dc949a730babda63e
gitgiant <prezidentbush@gmail.com>
2016-11-30 13:53:46 -0800

    Update tokens.py

--- bot/tokens.py
+++ bot/tokens.py
@@ -1,5 +1,8 @@
 # bot app credentials and oauth2 tokens for Reddit. keep it secret, keep it safe
 
+# \author Mattias Huber, Preston Peterson, Phillip Porter, Heather Bradfield, Zoltan Batoczki, Jesus Bamford
+# \copyright GNU Public License.
+
 app_key = '' # log in to the bot account on Reddit and go to https://ssl.reddit.com/prefs/apps if you need get the key
 app_secret = '' # log in to the bot account on Reddit and go to https://ssl.reddit.com/prefs/apps if you need to get the secret
 access_token = 'IMeh7iB7Q2knJlr764sTKuKThO4'
