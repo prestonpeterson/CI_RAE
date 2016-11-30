78a3ffbfc9ce19d351d9c933c078e3fa8c5e360c
gitgiant <prezidentbush@gmail.com>
2016-11-30 13:53:34 -0800

    Update settings.py

--- bot/settings.py
+++ bot/settings.py
@@ -1,5 +1,7 @@
 # bot user credentials for Reddit
 
+# \author Mattias Huber, Preston Peterson, Phillip Porter, Heather Bradfield, Zoltan Batoczki, Jesus Bamford
+# \copyright GNU Public License.
 scopes ='account creddits edit flair history identity livemanage modconfig modcontributors modflair modlog modothers modposts modself modwiki mysubreddits privatemessages read report save submit subscribe vote wikiedit wikiread'
 user_agent = 'Reddit Analytics Engine, writted by CSUCI students'
 user_name = 'ci_rae'
